fae4dacb82751644698f7cd83b6c2a459064f971
Mariusz-72 <mmbwdap@gmail.com>
2024-01-13 21:25:50 +0100

    update 3.1

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 
-import { store, persistor } from '../src/Redux/store';
+import { store, persistor } from './Redux/Store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
